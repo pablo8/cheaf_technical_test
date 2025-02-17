931dfb575fa0d8b77e21bf854c990f7fcc40bdaf
pablo <pablom.ochoteco@gmail.com>
2025-02-17 14:22:36 -0300

    + WIP: Adjustments for CI/CD process.
    + Pending:
     * Update readme

--- cheaf_test_tecnico/settings.py
+++ cheaf_test_tecnico/settings.py
@@ -226,8 +226,8 @@ USE_TZ = True
 # STATIC FILES
 # Archivos estáticos
 STATIC_URL = '/static/'
-
 STATIC_ROOT = "/app/static"
+STATICFILES_DIRS = ["/app/static/"]
 
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 
